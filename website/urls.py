24bed8f69505d1d38cea4e68678f2c1e9888b4b3
TealGibbon8 <alsadair.morrison1002@gmail.com>
2024-04-10 19:46:45 -0400

    Added projects and project picture pages.

--- website/urls.py
+++ website/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('copyright/', views.copyright, name='copyright'),
+    path('projects/', views.projects, name='projects'),
+    path('projects/<slug:slug>/', views.project, name='project'),
+    path('projects/add/', views.add_project, name='add_project'),
+    path('login/', views.user_login, name='login')
 ]
--- website/views.py
+++ website/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from datetime import datetime
+from website.forms import *
 
 # Create your views here.
 def index(request):
@@ -17,3 +18,36 @@ def about(request):
 def copyright(request):
     return render(request, "website/copyright.html")
 
+def projects(request):
+    projects = Project.objects.all()
+    return render(request, "website/projects.html", {'projects': projects})
+
+def project(request, slug):
+    project = Project.objects.get(slug=slug)
+    return render(request, "website/show_project.html", {'project': project})
+
+@login_required
+def add_project(request):
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('website:projects'))
+    else:
+        form = ProjectForm()
+    return render(request, "website/add_project.html", {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse('website:index'))
+        else:
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, "website/login.html")
